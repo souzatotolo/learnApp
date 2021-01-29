9c2f03e73978abb1f28ecfbe5627cd48fff115a2
Richard Totolo <61716244+souzatotolo@users.noreply.github.com>
2021-01-29 18:50:21 -0300

    Some colors corrections and implemented about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default function About() {
- return (
-   <div >
-     <div id="about" style={{background:'#fff', height:'100vh',display:'flex', alignItems:'center', justifyContent:'center'}}>
-       <p style={{fontSize: '2em', padding: 200, color: '#000', marginLeft:300,}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis felis at justo elementum. 
-      quis volutpat neque ultrices. Etiam sit amet mi sit amet nibh consequat maximus.</p> 
-     </div>
-   </div>
- );
+  return (
+    <div >
+      <div id="about" style={{ background: '#fff', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <p style={{ fontSize: '3.5em', marginLeft: 400, marginBottom: 200, fontWeight: 'bold' }}>Some information</p>
+        <p style={{ fontSize: '2em', padding: 100, color: '#000', }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis felis at justo elementum.
+      quis volutpat neque ultrices. Etiam sit amet mi sit amet nibh consequat maximus.</p>
+      </div>
+    </div>
+  );
 }
